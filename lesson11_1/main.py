2f909ee12b63e363cd96abce677d0991f9c34e1e
huang01199 <151027106+huang01199@users.noreply.github.com>
2023-12-19 13:06:23 +0000

    modify

--- lesson11_1/main.py
+++ lesson11_1/main.py
@@ -23,11 +23,16 @@ def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
 
     return students
 
+def saveToCSV(fileName:str,data:list[list]) -> None:
+    fileName += ".csv"
+    print("檔案名稱",fileName)
+    print(f"資料:{data}")
 
 
 if __name__ == '__main__':#判斷是不是正在執行主要檔
     s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
     students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
-    print(students)
 #append說明書https://www.runoob.com/python/att-list-append.html
+    fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
+    saveToCSV(fileName=fileName,data=students)
