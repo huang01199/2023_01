78e60d88b91d07f34d3b1040f011ce66a213b63e
huang01199 <151027106+huang01199@users.noreply.github.com>
2023-12-19 13:43:10 +0000

    modify

--- lesson11_3/outter/__init__.py
+++ lesson11_3/outter/__init__.py
@@ -1,5 +1,4 @@
-#自訂的tools module
-import random,csv
+import random
 
 def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
     '''
@@ -21,18 +20,3 @@ def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
         students.append(stu)
 
     return students
-
-def saveToCSV(fileName:str,data:list[list],subject_nums:int) -> bool:
-    fileName += ".csv"
-    subjects = [f'科目{i+1}' for i in range(subject_nums)]
-    fields = ['姓名']
-    fields.extend(subjects)
-    with open(fileName,mode='w',encoding='utf-8',newline='') as file:
-        try:
-            writer = csv.writer(file)
-            writer.writerow(fields)
-            writer.writerows(data)
-        except:
-            return False
-        else:
-            return True
